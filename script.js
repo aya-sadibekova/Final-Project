4ed75337e66fa98e3047e4eeea90ef8c7da483bf
aya-sadibekova <as5267@georgetown.edu>
2025-05-02 13:29:35 -0400

    Added game area and loading

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 const API_BASE = "https://collectionapi.metmuseum.org/public/collection/v1";
+let score = 0;
+
+const leftDiv = document.getElementById("leftArtwork");
+const rightDiv = document.getElementById("rightArtwork");
+const gameDiv = document.getElementById("game");
+const scoreDiv = document.getElementById("score");
+const gameOverDiv = document.getElementById("gameOver");
+function startGame() {
+  document.getElementById("startScreen").classList.add("d-none");
+  document.getElementById("header").classList.remove("d-none");
+  scoreDiv.classList.remove("d-none");
+  gameDiv.classList.remove("d-none");
+  score = 0;
+  shownIDs.clear();
+  scoreDiv.textContent = `Score: ${score}`;
+}
